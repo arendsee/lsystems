aa6ddf9102c084ca107aa0ea966f8b594cbe978b
Zebulun Arendsee <arendsee@iastate.edu>
2017-09-03 10:55:20 -0500

    Remove Syntax, Lexer, and Render modules
    The `Render` module was never really used for anything. Eventually I
    should move all diagram rendering to one module, but for now I don't
    want extra junk hanging around.
    
    The `Syntax` and `Lexer` modules where for parsing ABOP files, which is
    not longer a high priority. First I want to implement the full
    context-sensitive, stochastic, parameterized L-system. I want this
    linked into `diagrams` for visualization. After all this is working, I
    may consider resurecting the parser.

--- src/Lsystem.hs
+++ src/Lsystem.hs
@@ -2,13 +2,7 @@ module Lsystem
 (
     module Lsystem.Generator
   , module Lsystem.Turtle
-  , module Lsystem.Render
-  , module Lsystem.Syntax
-  , module Lsystem.Lexer
 ) where
 
 import Lsystem.Generator
 import Lsystem.Turtle
-import Lsystem.Render
-import Lsystem.Syntax
-import Lsystem.Lexer
