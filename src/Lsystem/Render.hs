d61e64fbbe38af66ebc84b3641f5aadeefa36b30
Zebulun Arendsee <arendsee@iastate.edu>
2017-09-12 07:32:23 -0500

    Logically incorrect solution reached
    A tree now forms, the types are right. The trig is just wrong. I'll sort
    this out later.

--- src/Lsystem/Render.hs
+++ src/Lsystem/Render.hs
@@ -11,7 +11,8 @@ import Lsystem.Generator
 import Lsystem.Grammar
 
 import Diagrams.Prelude
-import Diagrams.TwoD.Vector
+import qualified Diagrams.TwoD.Vector as TwoD
+import qualified Diagrams.ThreeD as ThreeD
 import Diagrams.Backend.SVG
 import qualified System.Random as SR
 
@@ -32,13 +33,13 @@ data Pacman = Pacman {
   , pacmanAngle :: (Double, Double, Double)
   , pacmanWidth :: Double
   , pacmanSpawn :: [Pacman]
-  , pacmanVectors :: [V2 Double]
+  , pacmanVectors :: [V3 Double]
 }
 
 pacman0 :: Pacman
 pacman0 = Pacman {
-      pacmanStart   = p3 (0,0,0) :: P3 Double
-    , pacmanEnd     = p3 (0,0,0) :: P3 Double
+      pacmanStart   = ThreeD.p3 (0,0,0) :: P3 Double
+    , pacmanEnd     = ThreeD.p3 (0,0,0) :: P3 Double
     , pacmanAngle   = (90,0,0)
     , pacmanWidth   = 1.0
     , pacmanSpawn   = []
@@ -56,15 +57,16 @@ spawnPacman p = Pacman {
   }
 
 eat :: Pacman -> Node -> Pacman 
-eat t (NodeRotate _ x1 y1 z1) = case (t pacmanAngle) of
+eat t (NodeRotate _ x1 y1 z1) = case (pacmanAngle t) of
   (x2,y2,z2) -> t { pacmanAngle = (x1+x2, y1+y2, z1+z2) }
 eat t (NodeDraw _ x) = t {
-      pacmanEnd = pacmanEnd t # translate v' # sized (mkWidth $ pacmanWidth t)
+      pacmanEnd = pacmanEnd t # ThreeD.translate v' -- # sized (mkWidth $ pacmanWidth t)
       -- build backwards for performance reasons, this will need to be reversed later
-    , pacmanVectors = v' : (pacmanVectors t)
+    , pacmanVectors = v' : pacmanVectors t
   } where
-    -- TODO: this needs to be projected on the XY plain
-    v' = e (pacmanAngle t) # scale x
+    v' = case pacmanAngle t of
+      -- FIXME: sort out the trig ...
+      (x,y,z) -> ThreeD.r3 ((cos y) * (cos z), (sin x) * (sin z), (sin x) * (sin y)) # scale x
 
 eat t (NodeDummy _ _) = t
 eat t (NodeWidth _ x) = t { pacmanWidth = x }
@@ -76,6 +78,15 @@ eat t (NodeBranch nss) =
 
 diagramPacman :: Pacman -> Diagram B
 diagramPacman p = mkDia p <> mergeSpawn p where
+  v3as2v (V3 x y _) = r2 (x, y)
+  p3as2p p = case (unp3 p) of
+    (x,y,_) -> p2 (x, y)
   -- reverse the vectors here, to rectify the backwards build in `eat`
-  mkDia = strokeLocTrail . (flip at) (pacmanStart p) . fromOffsets . reverse . pacmanVectors
+  mkDia
+    = strokeLocTrail
+    . (flip at) (p3as2p $ pacmanStart p)
+    . fromOffsets
+    . reverse
+    . map v3as2v
+    . pacmanVectors
   mergeSpawn = mconcat . map diagramPacman . pacmanSpawn
